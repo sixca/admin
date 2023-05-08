12ad67420a96e54fe3d7d87862ea8c5fd84f29aa
sixca <sixca0520@gmail.com>
2023-05-09 01:18:31 +0900

    0508 class commit

--- src/main/java/com/kbstar/ServletInitializer.java
+++ src/main/java/com/kbstar/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ws0504Application.class);
+		return application.sources(adminApplication.class);
 	}
 
 }
